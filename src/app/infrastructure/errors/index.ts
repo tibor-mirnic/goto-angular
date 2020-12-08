38ec167f5d326e3f33118e907e530c2ca46cd0e6
Tibor Mirnic <mirnictibor@gmail.com>
2020-12-08 20:15:59 +0100

    Added ProcessedErrorEventType to errors module export

--- src/app/infrastructure/errors/index.ts
+++ src/app/infrastructure/errors/index.ts
@@ -1,6 +1,8 @@
 export * from './errors.module';
 
 export * from './models/enum/error-type';
+export * from './models/enum/processed-error-event-type';
+
 export * from './models/base';
 export * from './models/application';
 export * from './models/user-friendly';
